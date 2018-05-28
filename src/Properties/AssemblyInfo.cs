146b71e48801a00f70ac196145b8cb671b32799f
Jan Schneider <schneider@zeiss-izm.de>
2018-05-28 08:58:44 +0200

    Increased version number and created NuGet package

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("PiWeb Formplots")]
-[assembly: AssemblyVersion( "1.2.1" )]
+[assembly: AssemblyVersion( "1.3.0" )]
 [assembly: AssemblyCompany( "Carl Zeiss Innovationszentrum für Messtechnik GmbH" )]
 [assembly: AssemblyProduct( "PiWeb" )]
 [assembly: AssemblyCopyright( "Copyright © 2017 Carl Zeiss Innovationszentrum für Messtechnik GmbH" )]
